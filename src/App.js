eeb25da409b305b9a8f98e90447a41e8229d6ff5
rob kurtz <rob@robkurtz.net>
2017-08-30 18:36:16 -0600

    add a few basic pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-const App = () => <div>Helloworldio</div>
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+
+const App = () => (
+  <div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+);
 
 export default App;
